e97831952287b3b0b33405a63ca1b837d6c9546a
Rohan Dewangan <dewanganrohan126@gmail.com>
2026-09-20 19:25:28 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for(char c : s.toCharArray()) {
+            if(c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            } else {
+                if(c == ')') {
+                    if(st.isEmpty() || st.pop() != '(') {
+                        return false;
+                    }
+                }
+                if(c == ']') {
+                    if(st.isEmpty() || st.pop() != '[') {
+                        return false;
+                    }
+                }
+                if(c == '}') {
+                    if(st.isEmpty() || st.pop() != '{') {
+                        return false;
+                    }
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+}
